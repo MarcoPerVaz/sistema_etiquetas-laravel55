1163d19bcf88d03e97d9aab28f8e101ba0518e73
Curso Git <marcooperdomo@gmail.com>
2019-12-05 11:15:20 -0600

    Bases (instalación y configuración)

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /* Componente para las etiquetas */ 
+        Conner\Tagging\Providers\TaggingServiceProvider::class,
+
     ],
 
     /*
