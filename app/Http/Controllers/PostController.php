c9dbbc8cdf4c56d96c6304ecd5aded78cd75c919
Curso Git <marcooperdomo@gmail.com>
2019-12-06 18:06:01 -0600

    Rutas, controlador y vista index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// Importado
+use App\Post;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Función que lista las etiquetas
+     */
+    public function index()
+    {
+
+        $posts = Post::orderBy( 'id', 'DESC' )->get();
+
+        return view( 'index', compact( 'posts' ) );
+
+    }
+
+    /**
+     * Función que almacena las etiquetas
+     */
+    public function store()
+    {
+        // Guardar
+    }
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Sistema de Etiquetas</title>
+
+    </head>
+    <body>
+        Página index
+    </body>
+</html>
